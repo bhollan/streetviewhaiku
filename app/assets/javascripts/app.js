4682092b1975562d3028616c6568a8acb4ea16ff
Brian Holland <beehollander@gmail.com>
2016-05-28 19:20:04 -0500

    re-routing

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,9 +3,24 @@ angular
     .config(function($stateProvider, $urlRouterProvider){
       $stateProvider
         .state('home',{
-            url: '/',
-            templateUrl: 'app/views/home.html',
-            controller: 'HomeController as home'
+          url: '/',
+          templateUrl: 'app/views/home.html',
+          controller: 'HomeController as home'
+        })
+        .state('words',{
+          url: '/words',
+          templateUrl: 'app/views/words.html',
+          controller: 'WordsController as words'
+        })
+        .state('words.browse',{
+          url: '/browse',
+          templateUrl: 'app/views/words/browse.html',
+          controller: 'BrowseWordsController as worder'
+        })
+        .state('words.check',{
+          url: '/check',
+          templateUrl: 'app/views/words/check.html',
+          controller: 'CheckWordsController as checker'
         });
       $urlRouterProvider.otherwise('/');
     });
