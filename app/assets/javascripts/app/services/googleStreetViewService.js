90e96ab6ebb1e6c158922e1b4269b1a090817234
Brian Holland <beehollander@gmail.com>
2016-05-30 17:02:57 -0500

    unsaved change left open

--- app/assets/javascripts/app/services/googleStreetViewService.js
+++ app/assets/javascripts/app/services/googleStreetViewService.js
@@ -1,8 +1,6 @@
 function googleStreetViewService($http){
   var ctrl = this;
 
-  this.BASE_URL = 'https://maps.googleapis.com/maps/api/streetview?size=600x300&heading=151.78&pitch=-0.76&location=';
-
   this.getStreetView = function(searchTerm){
     return $http.get(ctrl.BASE_URL + searchTerm)
       .then(function(response){
